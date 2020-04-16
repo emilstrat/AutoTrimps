41c01e10c8e83176c17f0f0f30517f0571868ed6
Emil <emil.strat@gmail.com>
2020-04-16 23:51:46 +0200

    Fix

--- modules/fight.js
+++ modules/fight.js
@@ -12,10 +12,10 @@ function betterAutoFight() {
     var newSquadRdy = game.resources.trimps.realMax() <= game.resources.trimps.owned + 1;
     var lowLevelFight = game.resources.trimps.maxSoldiers < breeding * 0.5 && breeding > game.resources.trimps.realMax() * 0.1 && game.global.world < 5;
     //Life Challenge
-    var livingStacks =  document.getElementById("livingStacks").textContent;
+    var livingStacks =  document.getElementById("livingStacks");
     if (game.global.challengeActive == "Life"
         && livingStacks
-        && getPageSetting('LifeMinUnliving') > livingStacks
+        && getPageSetting('LifeMinUnliving') > livingStacks.textContent
         && document.getElementsByClassName("cellColorCurrent")[0].classList.contains("Living")
         && game.global.currentMapId == ""){
             if (game.global.soldierHealth > 0){
